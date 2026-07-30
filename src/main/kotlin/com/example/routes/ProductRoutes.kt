276ff69c35ad166d4342e5ca9a8ca907c21ee2d2
DIEGO LOPEZ VARGAS <diegovarg998@gmail.com>
2026-07-30 12:02:05 -0600

    Tu mensaje

--- src/main/kotlin/com/example/routes/ProductRoutes.kt
+++ src/main/kotlin/com/example/routes/ProductRoutes.kt
@@ -10,6 +10,7 @@ import com.example.models.Compras
 import com.example.config.DatabaseFactory.dbQuery
 import io.ktor.client.*
 import io.ktor.client.call.*
+import io.ktor.client.statement.*
 import io.ktor.client.engine.cio.*
 import io.ktor.client.request.*
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
